29e5ed2e33ddb29ae34276ea3d2cc0a23b3e1f23
Manuel <angel.escobedoardn@uanl.edu.mx>
2026-08-27 08:46:38 -0600

    parte dos del laboratorio

---
+++ Laboratorio1parte2.R
@@ -0,0 +1,9 @@
+### Parte 2 
+# * **Nombre de estudiante:** Cualitativa 
+# * **Fecha de nacimiento:** Cualitativa 
+# * **Edad (en años):** Cuantitativa 
+# * **Direccion de casa:** Cualitativa
+# * **Grado de año universitario:** Cualitativa
+# * **Calificacion genral:** Cualitativa
+# * **Tiempo (en minutos para) completar el examen:** Cuantitativa
+# * **Numero de hermanos:** Cuantitativa 
