16a26359afafd38fe9ea68d8e9c14418e1d5b3c5
Manuel <angel.escobedoardn@uanl.edu.mx>
2026-09-03 09:10:10 -0600

    variables idependientes

--- PrimeraGrafica.R
+++ PrimeraGrafica.R
@@ -21,6 +21,41 @@ boxplot(Obs$IE ~ Obs$Tratamiento,
         ylab = "Indice (IE)",
         col = "green")
 
+# conocer la varianza de cada grupo
 
-     
+df_Ctrl <- subset(Obs, Tratamiento == "Ctrl")
+df_Fert <- subset(Obs, Tratamiento != "Ctrl")
+df_Fert <- subset(Obs, Tratamiento == "Fert")
 
+var(df_Ctrl$IE)
+var(df_Fert$IE)
+mean(df_Ctrl$IE)
+mean(df_Fert$IE)
+
+# La varianza del grupo fertilizado es 3 veces mayor que la varianza 
+# del grupo control 
+# Pregunta
+# ¿Seran las varianzas iguales o difrentes estadisticamente?
+
+#¿Proviene de una distribucion normal ambos grupos?
+shapiro.test(df_Ctrl$IE)
+# Grupo Crtl proviene de una distribucion normal 
+shapiro.test(df_Fert$IE)
+# Grupo Fert sigue una distribucion normal 
+
+
+# ¿seran las varianzas iguale o diferentes estadisticamente?
+
+var.test(df_Ctrl$IE, df_Fert$IE)
+
+# Las varianzas de ambos grupos son iguales
+
+
+# Exisiten difernecias entre los tratamientos 
+
+t.test(df_Ctrl$IE, df_Fert$IE, var.equal = TRUE)
+
+# si la pregunta es que Fert es mayor que ctrl 
+
+t.test(df_Fert$IE, df_Ctrl$IE,  var.equal = T,
+       alternative = "greater")
