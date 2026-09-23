e724ade67d6af9d2e2106e4481b7dc58b96611bb
Manuel <angel.escobedoardn@uanl.edu.mx>
2026-09-23 09:33:46 -0600

    Examen parte 1

---
+++ Examen parte 1.R
@@ -0,0 +1,35 @@
+# Datos sin valor extremo 
+x1 <- c(1.2, 1.5, 1.7, 1.8)
+
+# Resumen general 
+summary(x1)
+
+# Media 
+mean(x1)
+
+# Mediana 
+median(x1)
+
+# Varianza 
+var(x1)
+
+# Desviacion estandar 
+sd(x1)
+
+# Datos con valor extremo 
+x2 <- c(1.2, 1.5, 1.7, 1.8, 4.8)
+
+# Resumen general 
+summary(x2)
+
+# Media 
+mean(x2)
+
+# Mediana 
+median(x2)
+
+# Varianza 
+var(x2)
+
+# Desviacion estandar 
+sd(x2)
