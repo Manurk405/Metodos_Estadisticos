4239f57dfd3e30654693a91f400ada72b98322fc
Manuel <angel.escobedoardn@uanl.edu.mx>
2026-09-23 10:58:06 -0600

    qwq

--- ExamenPart2.R
+++ ExamenPart2.R
@@ -1,2 +1,14 @@
-anillos <- read.csv("Anillos_Parcial1.csv")
+anillos <- read.csv("Anillos_Parcial1 (2).csv")
+head(anillos)
+summary(anillos$AnchoAnillo_mm)
+
+hist(anillos$AnchoAnillo_mm,
+     col="forestgreen", border="white",
+     xlab="Ancho de anillo (mm)")
+
+sd(anillos$AnchoAnillo_mm)
+var(anillos$AnchoAnillo_mm)
+range(anillos$AnchoAnillo_mm)
+cv <- (sd(anillos$AnchoAnillo_mm)/mean(anillos$AnchoAnillo_mm))*100
+cv
 
